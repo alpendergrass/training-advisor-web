430e565373d322e62451ee8b24f6827be9b665a3
Albert Pendergrass <al.pendergrass@gmail.com>
2016-11-22 12:37:11 -0700

    Ensure that fitness is rising - slightly - in transition.

--- server/lib/advice-t0.js
+++ server/lib/advice-t0.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 var rules = [
   {
-    'name': 't0RestRule',
+    'name': 't0ChoiceRule',
     'priority': 3,
     'condition': function(R) {
       R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0' &&
@@ -11,9 +11,9 @@ var rules = [
       );
     },
     'consequence': function(R) {
-      this.plannedActivity.activityType = 'rest';
-      this.plannedActivity.rationale += ' t0RestRule.';
-      this.plannedActivity.advice += ' You went hard yesterday. Rest today.';
+      this.plannedActivity.activityType = 'choice';
+      this.plannedActivity.rationale += ' t0ChoiceRule.';
+      this.plannedActivity.advice += ' You should take a break from training. Now is a good time for cross-training. If you ride, keep it mellow and fun.';
       R.stop();
     }
   },
@@ -33,6 +33,21 @@ var rules = [
     }
   },
   {
+    'name': 't0ModerateRule',
+    'priority': 3,
+    'condition': function(R) {
+      R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0' &&
+        this.metricsOneDayPrior && this.metricsOneDayPrior.loadRating === 'rest'
+      );
+    },
+    'consequence': function(R) {
+      this.plannedActivity.activityType = 'moderate';
+      this.plannedActivity.rationale += ' t0ModerateRule.';
+      this.plannedActivity.advice += ' You should do a moderate workout today. Consider cross-training options. If you ride, keep it in zone 2.';
+      R.stop();
+    }
+  },
+  {
     'name': 't0HardRule',
     'priority': 2,
     'condition': function(R) {
@@ -45,20 +60,6 @@ var rules = [
       this.plannedActivity.activityType = 'hard';
       this.plannedActivity.rationale += ' t0HardRule.';
       this.plannedActivity.advice += ' Feel free to do a hard workout today and remember cross-training is an option.';
-      R.stop();
-    }
-  },
-  {
-    'name': 't0ChoiceRule',
-    'priority': 1,
-    'condition': function(R) {
-      R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0'
-      );
-    },
-    'consequence': function(R) {
-      this.plannedActivity.activityType = 'choice';
-      this.plannedActivity.rationale += ' t0ChoiceRule.';
-      this.plannedActivity.advice += ' You should take a break from training. Now is a good time for cross-training. If you ride, keep it mellow and fun.';
       R.stop();
     }
   }
