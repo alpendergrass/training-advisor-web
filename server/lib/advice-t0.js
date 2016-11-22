0966d96d938e437786caf4b9c638f432936d9158
Albert Pendergrass <al.pendergrass@gmail.com>
2016-11-22 13:07:40 -0700

    Revise transtion advice to eliminate hard workouts.

--- server/lib/advice-t0.js
+++ server/lib/advice-t0.js
@@ -3,21 +3,6 @@ var _ = require('lodash');
 
 var rules = [
   {
-    'name': 't0ChoiceRule',
-    'priority': 3,
-    'condition': function(R) {
-      R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0' &&
-        this.metricsOneDayPrior && this.metricsOneDayPrior.loadRating === 'hard'
-      );
-    },
-    'consequence': function(R) {
-      this.plannedActivity.activityType = 'choice';
-      this.plannedActivity.rationale += ' t0ChoiceRule.';
-      this.plannedActivity.advice += ' You should take a break from training. Now is a good time for cross-training. If you ride, keep it mellow and fun.';
-      R.stop();
-    }
-  },
-  {
     'name': 't0EasyRule',
     'priority': 3,
     'condition': function(R) {
@@ -48,21 +33,18 @@ var rules = [
     }
   },
   {
-    'name': 't0HardRule',
-    'priority': 2,
+    'name': 't0ChoiceRule',
+    'priority': 1,
     'condition': function(R) {
-      R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0' &&
-        this.metricsOneDayPrior && this.metricsOneDayPrior.loadRating !== 'hard' &&
-        this.metricsTwoDaysPrior && this.metricsTwoDaysPrior.loadRating !== 'hard'
-      );
+      R.when(this && !this.plannedActivity.activityType && this.trainingDay.period === 't0');
     },
     'consequence': function(R) {
-      this.plannedActivity.activityType = 'hard';
-      this.plannedActivity.rationale += ' t0HardRule.';
-      this.plannedActivity.advice += ' Feel free to do a hard workout today and remember cross-training is an option.';
+      this.plannedActivity.activityType = 'choice';
+      this.plannedActivity.rationale += ' t0ChoiceRule.';
+      this.plannedActivity.advice += ' You should take a break from training. Now is a good time for cross-training. If you ride, keep it mellow and fun.';
       R.stop();
     }
-  }
+  },
 ];
 
 module.exports = {};
