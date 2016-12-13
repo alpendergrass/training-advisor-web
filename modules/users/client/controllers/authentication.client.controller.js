5bd8bec2558416c03ccbefdee41abcf19e597323
Albert Pendergrass <al.pendergrass@gmail.com>
2016-12-12 17:14:22 -0700

    Convert auth to controller-as syntax.

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -2,33 +2,35 @@
 
 angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'PasswordValidator', 'Site', 'usSpinnerService',
   function ($scope, $state, $http, $location, $window, Authentication, PasswordValidator, Site, usSpinnerService) {
-    $scope.authentication = Authentication;
-    $scope.popoverMsg = PasswordValidator.getPopoverMsg();
+    var that = this;
+    this.authentication = Authentication;
+    this.popoverMsg = PasswordValidator.getPopoverMsg();
 
     // Get an eventual error defined in the URL query string:
-    $scope.error = $location.search().err;
+    this.error = $location.search().err;
 
     // If user is signed in then redirect back home
-    if ($scope.authentication.user) {
+    if (this.authentication.user) {
       $location.path('/');
     }
 
+    // TODO: convert function to arrow function so that I can use this.
     Site.get({}, function(site) {
       if (typeof site.allowRegistrations === 'undefined'){
-        $scope.allowRegistrations = true;
+        that.allowRegistrations = true;
       } else {
-        $scope.allowRegistrations = site.allowRegistrations;
+        that.allowRegistrations = site.allowRegistrations;
       }
     }, function(errorResponse) {
       if (errorResponse.data && errorResponse.data.message) {
-        $scope.error = errorResponse.data.message;
+        that.error = errorResponse.data.message;
       } else {
-        $scope.error = 'Server error prevented Site model retrieval.';
+        that.error = 'Server error prevented Site model retrieval.';
       }
     });
 
-    $scope.signup = function (isValid) {
-      $scope.error = null;
+    this.signup = function (isValid) {
+      this.error = null;
 
       if (!isValid) {
         $scope.$broadcast('show-errors-check-validity', 'userForm');
@@ -39,19 +41,19 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
 
       $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+        this.authentication.user = response;
 
         // And redirect to the previous or profile page
         // $state.go($state.previous.state.name || 'settings.profile', $state.previous.params);
         $state.go('settings.profile');
       }).error(function (response) {
         usSpinnerService.stop('authSpinner');
-        $scope.error = response.message;
+        this.error = response.message;
       });
     };
 
-    $scope.signin = function (isValid) {
-      $scope.error = null;
+    this.signin = function (isValid) {
+      this.error = null;
 
       if (!isValid) {
         $scope.$broadcast('show-errors-check-validity', 'userForm');
@@ -62,18 +64,18 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
 
       $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+        this.authentication.user = response;
 
         // And redirect to the previous or calendar page
         $state.go($state.previous.state.name || 'season', $state.previous.params);
       }).error(function (response) {
         usSpinnerService.stop('authSpinner');
-        $scope.error = response.message;
+        this.error = response.message;
       });
     };
 
     // OAuth provider request
-    $scope.callOauthProvider = function (url) {
+    this.callOauthProvider = function (url) {
       usSpinnerService.spin('authSpinner');
 
       if ($state.previous && $state.previous.href) {
