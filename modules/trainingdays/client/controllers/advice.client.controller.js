763bab789dd728412cfa4f5c0bd428c727b63f3b
Albert Pendergrass <al.pendergrass@gmail.com>
2016-12-13 23:57:58 -0700

    Implement CalendarController as.

--- modules/trainingdays/client/controllers/advice.client.controller.js
+++ modules/trainingdays/client/controllers/advice.client.controller.js
@@ -22,42 +22,42 @@ angular.module('trainingDays')
         this.datePickerStatus.opened = true;
       };
 
-      this.requestAdvice = function() {
-        var minAdviceDate = this.authentication.user.levelOfDetail > 2 ? null : today;
-        var maxAdviceDate = this.authentication.user.levelOfDetail > 2 ? null : moment().add(1, 'day').startOf('day').toDate();
+      // this.requestAdvice = function() {
+      var minAdviceDate = this.authentication.user.levelOfDetail > 2 ? null : today;
+      var maxAdviceDate = this.authentication.user.levelOfDetail > 2 ? null : moment().add(1, 'day').startOf('day').toDate();
 
-        this.getAdvice = function(isValid) {
-          this.error = null;
-
-          if (!isValid) {
-            $scope.$broadcast('show-errors-check-validity', 'trainingDayForm');
-            return false;
-          }
-
-          var getAdviceDate = moment(this.adviceDate).startOf('day').toDate();
-
-          TrainingDays.getAdvice({
-            trainingDate: getAdviceDate.toISOString(),
-            alternateActivity: null
-          }, function(trainingDay) {
-            $location.path('trainingDay/' + trainingDay._id);
-          }, function(errorResponse) {
-            if (errorResponse.data && errorResponse.data.message) {
-              this.error = errorResponse.data.message;
-            } else {
-              //Maybe this: errorResponse = Object {data: null, status: -1, config: Object, statusText: ''}
-              this.error = 'Server error prevented advice retrieval.';
-            }
-          });
-        };
-
-        this.adviceDateOptions = {
-          formatYear: 'yy',
-          startingDay: 1,
-          showWeeks: false,
-          minDate: minAdviceDate,
-          maxDate: maxAdviceDate
-        };
+      this.adviceDateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        showWeeks: false,
+        minDate: minAdviceDate,
+        maxDate: maxAdviceDate
       };
+
+      this.getAdvice = function(isValid) {
+        this.error = null;
+
+        if (!isValid) {
+          $scope.$broadcast('show-errors-check-validity', 'trainingDayForm');
+          return false;
+        }
+
+        var getAdviceDate = moment(this.adviceDate).startOf('day').toDate();
+        var that = this;
+
+        TrainingDays.getAdvice({
+          trainingDate: getAdviceDate.toISOString(),
+          alternateActivity: null
+        }, function(trainingDay) {
+          $location.path('trainingDay/' + trainingDay._id);
+        }, function(errorResponse) {
+          if (errorResponse.data && errorResponse.data.message) {
+            that.error = errorResponse.data.message;
+          } else {
+            that.error = 'Server error prevented advice retrieval.';
+          }
+        });
+      };
+      // };
     }
   ]);
--- modules/trainingdays/client/controllers/calendar.client.controller.js
+++ modules/trainingdays/client/controllers/calendar.client.controller.js
@@ -3,175 +3,169 @@
 angular.module('trainingDays')
   .controller('CalendarController', ['$scope', '$state', '$stateParams', '$location', '$compile', '$anchorScroll', 'Authentication', 'TrainingDays', 'Season', 'Util', '_', 'moment', 'usSpinnerService', 'MaterialCalendarData',
     function($scope, $state, $stateParams, $location, $compile, $anchorScroll, Authentication, TrainingDays, Season, Util, _, moment, usSpinnerService, MaterialCalendarData) {
-      $scope.authentication = Authentication;
+      this.authentication = Authentication;
 
       var jQuery = window.jQuery;
       angular.element(document).ready(function() {
         jQuery('[data-toggle="popover"]').popover();
       });
 
-      $scope._ = _;
-      $scope.today = moment().startOf('day').toDate();
+      var today = moment().startOf('day').toDate();
 
-      $scope.viewCalendar = function() {
-        var formatDayContent = function(trainingDay) {
-          var load = 0,
-            loadRating = '',
-            planActivity,
-            content = '<div class="td-calendar-content',
-            lengthOfFixedContent = 33;
+      var formatDayContent = function(trainingDay) {
+        var load = 0,
+          loadRating = '',
+          planActivity,
+          content = '<div class="td-calendar-content',
+          lengthOfFixedContent = 33;
 
-          //We want to be able to highlight today.
-          if (trainingDay.htmlID && trainingDay.htmlID === 'today') {
-            content += ' today-on-calendar';
-            lengthOfFixedContent += 18;
+        //We want to be able to highlight today.
+        if (trainingDay.htmlID && trainingDay.htmlID === 'today') {
+          content += ' today-on-calendar';
+          lengthOfFixedContent += 18;
+        }
+
+        content += '">';
+
+        content += trainingDay.name ? '<b>' + trainingDay.name + '</b> ' : '';
+        content += trainingDay.startingPoint ? '<b class="small text-danger">Season Start</b> ' : '';
+        content += trainingDay.fitnessAndFatigueTrueUp ? '<b class="small text-danger">Fitness and Fatigue True Up</b> ' : '';
+
+        content += '<small>';
+        lengthOfFixedContent += 7;
+
+        if (trainingDay.scheduledEventRanking) {
+          content += trainingDay.name ? ' - ' : '';
+
+          switch (trainingDay.scheduledEventRanking) {
+            case 1:
+              content += '<b class="text-danger">Goal Event!</b>';
+              break;
+            case 2:
+              content += '<b>Medium Priority Event</b>';
+              break;
+            case 3:
+              content += 'Low Priority Event';
+              break;
+            case 9:
+              content += 'Scheduled Off Day';
+              break;
+            default:
+              break;
           }
+        }
 
-          content += '">';
+        if (moment(trainingDay.date).isAfter(today, 'day')) {
+          content += content.length > lengthOfFixedContent ? '<br>' : '';
+          content += '<i>';
+          planActivity = Util.getPlannedActivity(trainingDay, 'plangeneration');
+          if (planActivity) {
+            content += Util.mapActivityTypeToVerbiage(planActivity.activityType) + ' - ';
+          }
+          content += 'load: ' + trainingDay.planLoad + '</i>';
+        }
 
-          content += trainingDay.name ? '<b>' + trainingDay.name + '</b> ' : '';
-          content += trainingDay.startingPoint ? '<b class="small text-danger">Season Start</b> ' : '';
-          content += trainingDay.fitnessAndFatigueTrueUp ? '<b class="small text-danger">Fitness and Fatigue True Up</b> ' : '';
 
-          content += '<small>';
-          lengthOfFixedContent += 7;
+        if (trainingDay.completedActivities.length > 0) {
+          content += content.length > lengthOfFixedContent ? '<br>' : '';
+          _.forEach(trainingDay.completedActivities, function(activity) {
+            load += activity.load;
+          });
+          loadRating = Util.getMetrics(trainingDay, 'actual').loadRating;
+          content += load ? ' Load: ' + load + ' - ' + loadRating + ' day' : '';
+        }
 
-          if (trainingDay.scheduledEventRanking) {
-            content += trainingDay.name ? ' - ' : '';
 
-            switch (trainingDay.scheduledEventRanking) {
-              case 1:
-                content += '<b class="text-danger">Goal Event!</b>';
-                break;
-              case 2:
-                content += '<b>Medium Priority Event</b>';
-                break;
-              case 3:
-                content += 'Low Priority Event';
-                break;
-              case 9:
-                content += 'Scheduled Off Day';
-                break;
-              default:
-                break;
+        // if (trainingDay.form !== 0) {
+        //   content += '<br><i>Form: ' + trainingDay.form + '</i>';
+        // }
+
+        // content += '<br><i>Period: ' + trainingDay.period + '</i>';
+
+        content += '</small></div>';
+        return content;
+      };
+
+      this.initCalendar = function() {
+        //Use vertical format if we are in a small window like on a phone.
+        if (jQuery(window).width() < 800) {
+          this.setDirection('vertical');
+          this.smallWindow = true;
+        } else {
+          this.smallWindow = false;
+        }
+        // Need to clear out calendar data. Moving goal date can strand some data otherwise.
+        MaterialCalendarData.data = {};
+
+        var that = this;
+
+        //We need to clean up any potential left over sim days.
+        TrainingDays.finalizeSim({
+          commit: 'no'
+        }, function(response) {
+          usSpinnerService.spin('tdSpinner');
+
+          Season.getSeason(function(errorMessage, season) {
+            usSpinnerService.stop('tdSpinner');
+            that.error = errorMessage;
+
+            if (season) {
+              that.season = season.days;
+              // Reload user object as notifications may have been updated.
+              Authentication.user = season.user;
+              that.hasStart = season.hasStart;
+              that.hasEnd = season.hasEnd;
+
+              _.forEach(season.days, function(td) {
+                MaterialCalendarData.setDayContent(td.date, formatDayContent(td));
+              });
             }
-          }
-
-          //Display future advice
-
-          if (moment(trainingDay.date).isAfter($scope.today, 'day')) {
-            content += content.length > lengthOfFixedContent ? '<br>' : '';
-            content += '<i>';
-            planActivity = Util.getPlannedActivity(trainingDay, 'plangeneration');
-            if (planActivity) {
-              content += Util.mapActivityTypeToVerbiage(planActivity.activityType) + ' - ';
-            }
-            content += 'load: ' + trainingDay.planLoad + '</i>';
-          }
-
-
-          if (trainingDay.completedActivities.length > 0) {
-            content += content.length > lengthOfFixedContent ? '<br>' : '';
-            _.forEach(trainingDay.completedActivities, function(activity) {
-              load += activity.load;
-            });
-            loadRating = Util.getMetrics(trainingDay, 'actual').loadRating;
-            content += load ? ' Load: ' + load + ' - ' + loadRating + ' day' : '';
-          }
-
-
-          // if (trainingDay.form !== 0) {
-          //   content += '<br><i>Form: ' + trainingDay.form + '</i>';
-          // }
-
-          // content += '<br><i>Period: ' + trainingDay.period + '</i>';
-
-          content += '</small></div>';
-          return content;
-        };
-
-        var initCalendar = function() {
-          //Use vertical format if we are in a small window like on a phone.
-          if (jQuery(window).width() < 800) {
-            $scope.setDirection('vertical');
-            $scope.smallWindow = true;
+          });
+        }, function(errorResponse) {
+          if (errorResponse.data && errorResponse.data.message) {
+            that.error = errorResponse.data.message;
           } else {
-            $scope.smallWindow = false;
+            that.error = 'Server error prevented simulation clean-up.';
           }
-          // Need to clear out calendar data. Moving goal date can strand some data otherwise.
-          MaterialCalendarData.data = {};
+        });
+      };
 
-          //We need to clean up any potential left over sim days.
-          TrainingDays.finalizeSim({
-            commit: 'no'
-          }, function(response) {
-            usSpinnerService.spin('tdSpinner');
-            $scope.isWorking = true;
+      this.setDirection = function(direction) {
+        this.direction = direction;
+        this.dayFormat = direction === 'vertical' ? 'EEE, MMM d' : 'd';
+      };
 
-            Season.getSeason(function(errorMessage, season) {
-              usSpinnerService.stop('tdSpinner');
-              $scope.isWorking = false;
-              $scope.error = errorMessage;
+      this.dayClick = function(date) {
+        var td = _.find(this.season, function(d) {
+          return (moment(d.date).isSame(moment(date), 'day'));
+        });
 
-              if (season) {
-                $scope.season = season.days;
-                // Reload user object as notifications may have been updated.
-                Authentication.user = season.user;
-                $scope.hasStart = season.hasStart;
-                $scope.hasEnd = season.hasEnd;
-                $scope.needsPlanGen = season.needsPlanGen;
+        if (td) {
+          $state.go('trainingDayView', { trainingDayId: td._id });
+        } else {
+          if (moment(date).isSameOrAfter(this.hasStart.date)) {
+            //trainingDay does not exist, we need to create it first.
+            var trainingDay = new TrainingDays({
+              date: date
+            });
 
-                _.forEach(season.days, function(td) {
-                  MaterialCalendarData.setDayContent(td.date, formatDayContent(td));
-                });
+            var that = this;
+
+            trainingDay.$create(function(trainingDay) {
+              // Reload user to pick up changes in notifications.
+              Authentication.user = trainingDay.user;
+              $location.path('trainingDay/' + trainingDay._id);
+            }, function(errorResponse) {
+              if (errorResponse.data && errorResponse.data.message) {
+                that.error = errorResponse.data.message;
+              } else {
+                that.error = 'Server error prevented trainingDay creation.';
               }
             });
-          }, function(errorResponse) {
-            if (errorResponse.data && errorResponse.data.message) {
-              $scope.error = errorResponse.data.message;
-            } else {
-              $scope.error = 'Server error prevented simulation clean-up.';
-            }
-          });
-        };
-
-        $scope.setDirection = function(direction) {
-          $scope.direction = direction;
-          $scope.dayFormat = direction === 'vertical' ? 'EEE, MMM d' : 'd';
-        };
-
-        $scope.dayClick = function(date) {
-          var td = _.find($scope.season, function(d) {
-            return (moment(d.date).isSame(moment(date), 'day'));
-          });
-
-          if (td) {
-            $state.go('trainingDayView', { trainingDayId: td._id });
-          } else {
-            if (moment(date).isSameOrAfter($scope.hasStart.date)) {
-              //trainingDay does not exist, we need to create it first.
-              var trainingDay = new TrainingDays({
-                date: date
-              });
-
-              trainingDay.$create(function(trainingDay) {
-                // Reload user to pick up changes in notifications.
-                Authentication.user = trainingDay.user;
-                $location.path('trainingDay/' + trainingDay._id);
-              }, function(errorResponse) {
-                if (errorResponse.data && errorResponse.data.message) {
-                  $scope.error = errorResponse.data.message;
-                } else {
-                  //Maybe this: errorResponse = Object {data: null, status: -1, config: Object, statusText: ''}
-                  $scope.error = 'Server error prevented trainingDay creation.';
-                }
-              });
-            }
           }
-        };
-
-        initCalendar();
+        }
       };
+
 
       // //Highlight today in calendar view
       // //Note that this will not survive a change in calendar layout (calendar to agenda or v.v.).
