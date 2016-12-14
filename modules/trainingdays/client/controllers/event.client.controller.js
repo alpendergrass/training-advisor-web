be6d990413bcd454c70cadc2a7bc89e17c020a98
Albert Pendergrass <al.pendergrass@gmail.com>
2016-12-14 00:07:00 -0700

    Remove scheduleEvent method.

--- modules/trainingdays/client/controllers/event.client.controller.js
+++ modules/trainingdays/client/controllers/event.client.controller.js
@@ -10,8 +10,6 @@ angular.module('trainingDays')
         jQuery('[data-toggle="popover"]').popover();
       });
 
-      $scope._ = _;
-
       $scope.datePickerStatus = {
         opened: false
       };
@@ -20,166 +18,164 @@ angular.module('trainingDays')
         $scope.datePickerStatus.opened = true;
       };
 
-      $scope.scheduleEvent = function() {
+      var minEventDate = $scope.authentication.user.levelOfDetail > 2 ? null : moment().startOf('day').toDate();
+
+      $scope.eventDateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        showWeeks: false,
+        minDate: minEventDate
+      };
+
+      $scope.recurrenceSpec = null;
+
+      //If the user clicks a Create Goal link we pass in the event priority so no need to ask.
+      if ($stateParams.scheduledEventRanking) {
+        $scope.scheduledEventRanking = $stateParams.scheduledEventRanking;
+        $scope.eventRankingParm = $stateParams.scheduledEventRanking;
+      } else {
+        $scope.eventRankingParm = 0;
+      }
+
+      $scope.$watch('scheduledEventRanking', function(ranking) {
+        if (ranking !== '1') {
+          $scope.estimatedLoad = 0;
+          $scope.eventTerrain = 0;
+        }
+      });
+
+      $scope.checkRecurrence = function() {
         $scope.recurrenceSpec = null;
 
-        //If the user clicks a Create Goal link we pass in the event prioity so no need to ask.
-        if ($stateParams.scheduledEventRanking) {
-          $scope.scheduledEventRanking = $stateParams.scheduledEventRanking;
-          $scope.eventRankingParm = $stateParams.scheduledEventRanking;
-        } else {
-          $scope.eventRankingParm = 0;
+        if ($scope.recurs) {
+          $scope.openRecurrence($scope.date);
         }
+      };
 
-        var minEventDate = $scope.authentication.user.levelOfDetail > 2 ? null : moment().startOf('day').toDate();
+      $scope.openRecurrence = function(eventDate) {
+        var modalInstance = $uibModal.open({
+          templateUrl: 'recurrance.html',
+          //size: 'sm',
+          controller: ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+            $scope.recurrenceSpec = {
+              daysOfWeek: {}
+            };
 
-        $scope.eventDateOptions = {
-          formatYear: 'yy',
-          startingDay: 1,
-          showWeeks: false,
-          minDate: minEventDate
-        };
+            $scope.recurrenceSpec.summary = '';
 
-        $scope.$watch('scheduledEventRanking', function(ranking) {
-          if (ranking !== '1') {
-            $scope.estimatedLoad = 0;
-            $scope.eventTerrain = 0;
-          }
-        });
+            var minRepeatDate = moment(eventDate).add(1, 'day').startOf('day').toDate();
+            var maxRepeatDate = moment().add(52, 'weeks').startOf('day').toDate();
 
-        $scope.checkRecurrence = function() {
-          $scope.recurrenceSpec = null;
+            $scope.repeatDateOptions = {
+              formatYear: 'yy',
+              startingDay: 1,
+              showWeeks: false,
+              minDate: minRepeatDate,
+              maxDate: maxRepeatDate
+            };
 
-          if ($scope.recurs) {
-            $scope.openRecurrence($scope.date);
-          }
-        };
 
-        $scope.openRecurrence = function(eventDate) {
-          var modalInstance = $uibModal.open({
-            templateUrl: 'recurrance.html',
-            //size: 'sm',
-            controller: ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
-              $scope.recurrenceSpec = {
-                daysOfWeek: {}
-              };
+            $scope.datePickerStatus = {
+              opened: false
+            };
+
+            $scope.openDatePicker = function($event) {
+              $scope.datePickerStatus.opened = true;
+            };
+
+            $scope.noDaysSelected = function() {
+              return !_.find($scope.recurrenceSpec.daysOfWeek, function(o) {
+                return o === true;
+              });
+            };
+
+            $scope.formatRepeatSummary = function($event) {
+              var dayOfWeek,
+                selectedDays = '';
 
               $scope.recurrenceSpec.summary = '';
 
-              var minRepeatDate = moment(eventDate).add(1, 'day').startOf('day').toDate();
-              var maxRepeatDate = moment().add(52, 'weeks').startOf('day').toDate();
+              if (parseInt($scope.recurrenceSpec.everyNTimeUnits, 10) === 1) {
+                $scope.recurrenceSpec.summary = 'Weekly';
+              } else if ($scope.recurrenceSpec.everyNTimeUnits > 1) {
+                $scope.recurrenceSpec.summary = 'Every ' + $scope.recurrenceSpec.everyNTimeUnits + ' weeks';
+              }
 
-              $scope.repeatDateOptions = {
-                formatYear: 'yy',
-                startingDay: 1,
-                showWeeks: false,
-                minDate: minRepeatDate,
-                maxDate: maxRepeatDate
-              };
-
-
-              $scope.datePickerStatus = {
-                opened: false
-              };
-
-              $scope.openDatePicker = function($event) {
-                $scope.datePickerStatus.opened = true;
-              };
-
-              $scope.noDaysSelected = function() {
-                return !_.find($scope.recurrenceSpec.daysOfWeek, function(o) {
-                  return o === true;
-                });
-              };
-
-              $scope.formatRepeatSummary = function($event) {
-                var dayOfWeek,
-                  selectedDays = '';
-
-                $scope.recurrenceSpec.summary = '';
-
-                if (parseInt($scope.recurrenceSpec.everyNTimeUnits, 10) === 1) {
-                  $scope.recurrenceSpec.summary = 'Weekly';
-                } else if ($scope.recurrenceSpec.everyNTimeUnits > 1) {
-                  $scope.recurrenceSpec.summary = 'Every ' + $scope.recurrenceSpec.everyNTimeUnits + ' weeks';
+              _.forEach($scope.recurrenceSpec.daysOfWeek, function(value, key) {
+                if (value) {
+                  dayOfWeek = _.find($scope.daysOfWeek, { 'value': key });
+                  selectedDays += dayOfWeek.title + ', ';
                 }
+              });
 
-                _.forEach($scope.recurrenceSpec.daysOfWeek, function(value, key) {
-                  if (value) {
-                    dayOfWeek = _.find($scope.daysOfWeek, { 'value': key });
-                    selectedDays += dayOfWeek.title + ', ';
-                  }
-                });
+              if (selectedDays) {
+                $scope.recurrenceSpec.summary += ' on ' + selectedDays.substring(0, selectedDays.length - 2);
+              }
 
-                if (selectedDays) {
-                  $scope.recurrenceSpec.summary += ' on ' + selectedDays.substring(0, selectedDays.length - 2);
-                }
+              if ($scope.recurrenceSpec.endsOn) {
+                $scope.recurrenceSpec.summary += ' until ' + moment($scope.recurrenceSpec.endsOn).format('dddd, MMMM Do YYYY');
+              }
+            };
 
-                if ($scope.recurrenceSpec.endsOn) {
-                  $scope.recurrenceSpec.summary += ' until ' + moment($scope.recurrenceSpec.endsOn).format('dddd, MMMM Do YYYY');
-                }
-              };
+            $scope.daysOfWeek = [
+              { text: 'S', value: '0', title: 'Sunday' },
+              { text: 'M', value: '1', title: 'Monday' },
+              { text: 'T', value: '2', title: 'Tuesday' },
+              { text: 'W', value: '3', title: 'Wednesday' },
+              { text: 'T', value: '4', title: 'Thursday' },
+              { text: 'F', value: '5', title: 'Friday' },
+              { text: 'S', value: '6', title: 'Saturday' }
+            ];
 
-              $scope.daysOfWeek = [
-                { text: 'S', value: '0', title: 'Sunday' },
-                { text: 'M', value: '1', title: 'Monday' },
-                { text: 'T', value: '2', title: 'Tuesday' },
-                { text: 'W', value: '3', title: 'Wednesday' },
-                { text: 'T', value: '4', title: 'Thursday' },
-                { text: 'F', value: '5', title: 'Friday' },
-                { text: 'S', value: '6', title: 'Saturday' }
-              ];
+            $scope.saveRecurrence = function() {
+              $uibModalInstance.close($scope.recurrenceSpec);
+            };
 
-              $scope.saveRecurrence = function() {
-                $uibModalInstance.close($scope.recurrenceSpec);
-              };
+            $scope.cancelRecurrence = function() {
+              $uibModalInstance.dismiss('cancel');
+            };
+          }]
+        });
 
-              $scope.cancelRecurrence = function() {
-                $uibModalInstance.dismiss('cancel');
-              };
-            }]
-          });
+        modalInstance.result.then(function(recurrenceSpec) {
+          $scope.recurrenceSpec = recurrenceSpec;
+        }, function() {
+          //User cancelled out of dialog.
+          $scope.recurs = false;
+          $scope.recurrenceSpec = null;
+        }).finally(function() {
+        });
+      };
 
-          modalInstance.result.then(function(recurrenceSpec) {
-            $scope.recurrenceSpec = recurrenceSpec;
-          }, function() {
-            //User cancelled out of dialog.
-            $scope.recurs = false;
-            $scope.recurrenceSpec = null;
-          }).finally(function() {
-          });
-        };
+      $scope.createEvent = function(isValid) {
+        $scope.error = null;
 
-        $scope.createEvent = function(isValid) {
-          $scope.error = null;
+        if (!isValid) {
+          $scope.$broadcast('show-errors-check-validity', 'trainingDayForm');
 
-          if (!isValid) {
-            $scope.$broadcast('show-errors-check-validity', 'trainingDayForm');
+          return false;
+        }
 
-            return false;
+        var trainingDay = new TrainingDays({
+          date: this.date,
+          name: this.name,
+          estimatedLoad: this.estimatedLoad,
+          eventTerrain: this.eventTerrain,
+          scheduledEventRanking: this.scheduledEventRanking,
+          recurrenceSpec: this.recurrenceSpec,
+          notes: this.notes
+        });
+
+        trainingDay.$create(function(response) {
+          $state.go('season');
+        }, function(errorResponse) {
+          if (errorResponse.data && errorResponse.data.message) {
+            $scope.error = errorResponse.data.message;
+          } else {
+            //Maybe this: errorResponse = Object {data: null, status: -1, config: Object, statusText: ''}
+            $scope.error = 'Server error prevented event creation.';
           }
-
-          var trainingDay = new TrainingDays({
-            date: this.date,
-            name: this.name,
-            estimatedLoad: this.estimatedLoad,
-            eventTerrain: this.eventTerrain,
-            scheduledEventRanking: this.scheduledEventRanking,
-            recurrenceSpec: this.recurrenceSpec,
-            notes: this.notes
-          });
-
-          trainingDay.$create(function(response) {
-            $state.go('season');
-          }, function(errorResponse) {
-            if (errorResponse.data && errorResponse.data.message) {
-              $scope.error = errorResponse.data.message;
-            } else {
-              //Maybe this: errorResponse = Object {data: null, status: -1, config: Object, statusText: ''}
-              $scope.error = 'Server error prevented event creation.';
-            }
-          });
-        };
+        });
       };
     }
   ]);
