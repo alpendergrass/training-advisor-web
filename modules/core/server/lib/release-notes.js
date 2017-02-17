274efdb27bd7af13da51471d781144ad834e3578
Albert Pendergrass <al.pendergrass@gmail.com>
2017-02-17 12:22:35 -0700

    merge.

--- modules/core/server/lib/release-notes.js
+++ modules/core/server/lib/release-notes.js
@@ -18,26 +18,6 @@ var releaseNotes = [
     title: '',
     message: '',
     features: [
-      'We now retain your previous threshold power settings. See My Profile.',
-      'When Syncing with Strava, we can now specify that we should replace existing rides in Tacit Training with the corresponding rides from Strava, ensuring that the appropriate FTP is used when processing the rides.'
-    ],
-    fixes: []
-  },
-  {
-    version: '0.6.10',
-    title: '',
-    message: 'Fixed a few bugs.',
-    features: [],
-    fixes: [
-      'When setting up sim are we reloading chart after changing day. Stop, it takes too long.',
-      'Update of xeditable or something related broke xeditable radio button value validations.'
-    ]
-  },
-  {
-    version: '0.6.9',
-    title: '',
-    message: '',
-    features: [
       'List new features when a new version of the app is loaded. (This :)'
     ],
     fixes: []
