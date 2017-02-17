c1c99c8da8878e8f10fc917a0ba610721aec7eee
Albert Pendergrass <al.pendergrass@gmail.com>
2017-02-17 12:25:27 -0700

    merge!

--- modules/core/server/lib/release-notes.js
+++ modules/core/server/lib/release-notes.js
@@ -2,18 +2,6 @@
 
 var releaseNotes = [
   {
-    version: '0.7.0',
-    title: '',
-    message: '',
-    features: [
-      'We now retain your previous threshold power settings. See My Profile.',
-      'When Syncing with Strava, we can now specify that we should replace existing rides in Tacit Training with the corresponding rides from Strava, ensuring that the appropriate FTP is used when processing the rides.'
-    ],
-    fixes: [
-      ''
-    ]
-  },
-  {
     version: '0.6.9',
     title: '',
     message: '',
