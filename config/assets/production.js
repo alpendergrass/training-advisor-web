e76a259f07d047251845f3cdfd6eec2bd9e93bd3
Albert Pendergrass <al.pendergrass@gmail.com>
2017-04-28 20:51:27 -0600

    Archive old migration scripts.

--- config/assets/production.js
+++ config/assets/production.js
@@ -46,6 +46,6 @@ module.exports = {
       ]
     },
     css: 'public/dist/application-7e41a58ea8.min.css',
-    js: 'public/dist/application-25916af14a.min.js'
+    js: 'public/dist/application-d37895b6fa.min.js'
   }
 };
